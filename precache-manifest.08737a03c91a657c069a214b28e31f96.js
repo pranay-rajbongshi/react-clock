02937d32c2ad366a6ad9fe49d88ee5cb2337186d
pranay-rajbongshi <pranay@naahor.com>
2018-12-19 12:39:16 +0530

    Updates

---
+++ precache-manifest.08737a03c91a657c069a214b28e31f96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-clock/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6e3cf1163772f9f8b86a",
+    "url": "/react-clock/static/js/runtime~main.6e3cf116.js"
+  },
+  {
+    "revision": "86f52397548c0277b69e",
+    "url": "/react-clock/static/js/main.86f52397.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/react-clock/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "86f52397548c0277b69e",
+    "url": "/react-clock/static/css/main.3e522f46.chunk.css"
+  },
+  {
+    "revision": "e3c0a5f6ca22552d423f80d9a5b54f9f",
+    "url": "/react-clock/index.html"
+  }
+];
